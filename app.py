755624ce9609fe80676a72a93e0c38d9b69bd514
gaba42 <106816837+gaba42@users.noreply.github.com>
2024-02-20 15:06:40 +0900

    Extract text from PDFs

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Chat with PDFs", page_icon=':books:')
 
     st.header("Multiple PDFs ChatBot :books:")
@@ -8,8 +22,18 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click 'Process'")
-        st.button("Process")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):  # to make it user friendly
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+
+                # get the text chuncks
+
+                # create vector store with embeddings
+
 
 if __name__ == '__main__':
     main()
